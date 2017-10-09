f1c7bb824d98abbce7b04880375ae82dce60e4eb
John Wu <johnwu93@gmail.com>
2017-10-09 14:08:10 -0700

    refactor: Refactor styling starter code
    Refactored code to use bootstrap and sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,36 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/scripts/main.js',
+  entry: ['./src/scripts/main.js', './src/styles/styles.scss'],
   resolve: {
     alias: {
-      jquery: 'jquery/src/jquery',
+      jquery: 'jquery/src/jquery'
     },
-    extensions: ['.js'],
+    extensions: ['.js']
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build')
   },
   devtool: 'source-map',
+  module: {
+    rules: [
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1'
+        })
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'dist/[name].bundle.css',
+      allChunks: true,
+    })
+  ]
 };
